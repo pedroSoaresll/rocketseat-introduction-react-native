9b0bb489eff6162ac6865a6a20834e875850a2ec
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2019-12-26 12:19:59 -0300

    add proptypes + doing navigation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Keyboard, ActivityIndicator } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import PropTypes from 'prop-types';
 import api from '../../services/api';
 
 import {
@@ -65,8 +66,14 @@ export default class Main extends Component {
     Keyboard.dismiss();
   };
 
+  handleNavigate = item => {
+    const { navigation } = this.props;
+
+    navigation.navigate('User', { user: item });
+  };
+
   render() {
-    const { users, newUser, loading } = this.state;
+    const { users, loading } = this.state;
 
     return (
       <Container>
@@ -98,7 +105,7 @@ export default class Main extends Component {
               <Name>{item.name}</Name>
               <Bio>{item.bio}</Bio>
 
-              <ProfileButton onPress={() => { }}>
+              <ProfileButton onPress={() => this.handleNavigate(item)}>
                 <ProfileButtonText>Ver perfil</ProfileButtonText>
               </ProfileButton>
             </User>
@@ -111,4 +118,10 @@ export default class Main extends Component {
 
 Main.navigationOptions = {
   title: 'Usuários',
+};
+
+Main.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
 };
--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -3,6 +3,10 @@ import { View } from 'react-native';
 
 // import { Container } from './styles';
 
-export default function User() {
+export default function User(props) {
+  const { navigation } = props;
+
+  console.tron.log('console dentro do user', navigation.getParam('user'));
+
   return <View />;
 }
