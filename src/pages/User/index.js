3f9a3f14cfad25ad0698b6096e6a46476567d18c
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2019-12-26 13:18:10 -0300

    load more starred repos when user going to list end

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,12 +1,100 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { ActivityIndicator } from 'react-native';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+  AreaActivityIndicator,
+} from './styles';
 
-export default function User(props) {
-  const { navigation } = props;
+export default class User extends Component {
+  state = {
+    stars: [],
+    loading: false,
+    page: 1,
+  };
 
-  console.tron.log('console dentro do user', navigation.getParam('user'));
+  async componentDidMount() {
+    this.setState({ loading: true });
 
-  return <View />;
+    const { data } = await this.getStarredRepos();
+
+    this.setState({ stars: data, loading: false });
+  }
+
+  handleEndReached = async () => {
+    const { page, stars } = this.state;
+
+    const nextPage = page + 1;
+
+    const { data } = await this.getStarredRepos(nextPage);
+
+    this.setState({ stars: [...stars, ...data], page: nextPage });
+  };
+
+  getStarredRepos = (page = 1) => {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    return api.get(`/users/${user.login}/starred?page=${page}`);
+  };
+
+  render() {
+    const { navigation } = this.props;
+    const { stars, loading } = this.state;
+
+    const user = navigation.getParam('user');
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        {loading ? (
+          <AreaActivityIndicator>
+            <ActivityIndicator color="#333" size={30} />
+          </AreaActivityIndicator>
+        ) : (
+            <Stars
+              data={stars}
+              keyExtractor={star => String(star.id)}
+              onEndReached={this.handleEndReached}
+              renderItem={({ item }) => (
+                <Starred>
+                  <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                  <Info>
+                    <Title>{item.name}</Title>
+                    <Author>{item.owner.login}</Author>
+                  </Info>
+                </Starred>
+              )}
+            />
+          )}
+      </Container>
+    );
+  }
 }
+
+User.navigationOptions = ({ navigation }) => ({
+  title: navigation.getParam('user').name,
+});
+
+User.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+  }).isRequired,
+};
